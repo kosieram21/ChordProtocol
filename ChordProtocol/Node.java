4f295c85b0a7ae40ef08ff284f2653cd1057814c
Mitch Kosieradzki <mitchkos21@gmail.com>
2021-04-13 22:45:26 -0500

    Mark which INode methods shane needs to implement

--- ChordProtocol/Node.java
+++ ChordProtocol/Node.java
@@ -5,17 +5,17 @@ import java.rmi.RemoteException;
 public class Node implements INode {
     @Override
     public String findSuccessor(int key, boolean traceFlag) throws RemoteException {
-        return null;
+        return null; // Shane implement
     }
 
     @Override
     public String findPredecessor(int key) throws RemoteException {
-        return null;
+        return null; // Shane implement
     }
 
     @Override
     public String closestPrecedingFinger(int key) throws RemoteException {
-        return null;
+        return null; // Shane implement
     }
 
     @Override
@@ -40,12 +40,12 @@ public class Node implements INode {
 
     @Override
     public boolean insert(String word, String definition) throws RemoteException {
-        return false;
+        return false; // Shane implement
     }
 
     @Override
     public String lookup(String word) throws RemoteException {
-        return null;
+        return null; // Shane implement
     }
 
     @Override
