ef97240a0ee929b03a0bf06208bdbf2dd522d4a4
Mitch Kosieradzki <mitchkos21@gmail.com>
2021-04-20 19:47:09 -0500

    print service name

--- ChordProtocol/Client.java
+++ ChordProtocol/Client.java
@@ -7,6 +7,7 @@ public class Client {
         int port = Integer.parseInt(args[0]);
         for(int i = 1; i < args.length; i++) {
             String service_name = String.format("//%s:%d/%s", args[i], port, "Chord");
+            System.out.println(service_name);
             INode node = (INode) Naming.lookup(service_name);
             System.out.println("----------------------------------------------------");
             System.out.println(node.printFingerTable());
