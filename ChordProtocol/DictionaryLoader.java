2d7a73bde9ce09372400daf8f552b4e09dde6f04
Shane Kosieradzki <shanekos1@gmail.com>
2021-04-22 14:39:13 -0500

    [Shane] (WIP) Created interface and added text file to repo

---
+++ ChordProtocol/DictionaryLoader.java
@@ -0,0 +1,24 @@
+package ChordProtocol;
+
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.util.Scanner;
+
+public class DictionaryLoader {
+    public DictionaryLoader(String filename) {
+//        File file = new File(filename);
+//        Scanner reader = new Scanner(file);
+//        while (reader.hasNextLine())
+//        {
+//            String line = reader.nextLine();
+//            String[] splitLine = line.split(":");
+//            String word = splitLine[0];
+//            String definition = splitLine[1];
+//        }
+//        myReader.close();
+
+    }
+
+    static Pair<String, String>[]
+
+}
---
+++ ChordProtocol/URL_Loader.java
@@ -0,0 +1,11 @@
+package ChordProtocol;
+
+public class URL_Loader {
+    public URL_Loader(String filePath) {
+
+    }
+
+    public String[] getURLs() {
+        return null;
+    }
+}
