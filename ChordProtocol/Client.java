c5478698d3442675fa0e7e0a64fae574c101510b
Mitch Kosieradzki <mitchkos21@gmail.com>
2021-04-22 15:17:16 -0500

    client and loader programs implemented

--- ChordProtocol/Client.java
+++ ChordProtocol/Client.java
@@ -2,21 +2,23 @@ package ChordProtocol;
 
 import java.rmi.Naming;
 import java.util.List;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws Exception {
         int port = Integer.parseInt(args[0]);
-        List<String> urls = URL_Loader.load(args[1]);
-        List<String[]> definitions = DictionaryLoader.load(args[2]);
+        String url = args[1];
 
-        for(String url : urls)
-            System.out.println(url);
+        String service_name = String.format("//%s:%d/%s", url, port, "Chord");
+        INode node = (INode) Naming.lookup(service_name);
 
-        System.out.println("----------------------------------------------------");
-
-        assert definitions != null;
-        for(String[] definition : definitions)
-            System.out.printf("%s: %s%n", definition[0], definition[1]);
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            System.out.println("Enter word to lookup (type exit to escape): ");
+            String input = scanner.nextLine();
+            if(input.equals("exit")) break;
+            System.out.printf("Result: %s%n", node.lookup(input));
+        }
 
         /*for(int i = 1; i < args.length; i++) {
             String service_name = String.format("//%s:%d/%s", args[i], port, "Chord");
--- ChordProtocol/DictionaryLoader.java
+++ ChordProtocol/DictionaryLoader.java
@@ -2,11 +2,24 @@ package ChordProtocol;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.rmi.Naming;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class DictionaryLoader {
+    public static void main(String[] args) throws Exception {
+        int port = Integer.parseInt(args[0]);
+        String url = args[1];
+        List<String[]> definitions = DictionaryLoader.load(args[2]);
+
+        String service_name = String.format("//%s:%d/%s", url, port, "Chord");
+        INode node = (INode) Naming.lookup(service_name);
+
+        for(String[] definition : definitions)
+            node.insert(definition[0], definition[1]);
+    }
+
     public static List<String[]> load(String filePath) throws FileNotFoundException {
         File file = new File(filePath);
         Scanner scanner = new Scanner(file);
